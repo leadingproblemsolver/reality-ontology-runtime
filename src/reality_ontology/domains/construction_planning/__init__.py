0d6f117916761b107aa702b2936a5f8d4e9d83b5
lps_atwork <leadingproblemsolver@gmail.com>
2026-09-04 00:04:51 +0300

    feat: expose construction planning domain

---
+++ src/reality_ontology/domains/construction_planning/__init__.py
@@ -0,0 +1,27 @@
+from .core import (
+    Activity,
+    ActivityRelation,
+    ConstructionInputError,
+    ProjectionRow,
+    Requirement,
+    diff_activities,
+    load_activities,
+    load_relations,
+    load_requirements,
+    project_lookahead,
+    run_lookahead,
+)
+
+__all__ = [
+    "Activity",
+    "ActivityRelation",
+    "ConstructionInputError",
+    "ProjectionRow",
+    "Requirement",
+    "diff_activities",
+    "load_activities",
+    "load_relations",
+    "load_requirements",
+    "project_lookahead",
+    "run_lookahead",
+]
